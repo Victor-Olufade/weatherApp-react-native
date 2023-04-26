e6888b66be85e2a6c194d4e11d7cd14dc6d3c7e0
Victor-Olufade <olufadevictor97@gmail>
2023-04-26 17:15:20 +0100

    initialize App; add basic layout

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  doubleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "none",
+  semi: false
+};
--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+      <Text>Current Weather</Text>
     </View>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
+  wrapper: { flex: 1 },
   container: {
+    backgroundColor: "pink",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    alignItems: "center" 
+  }
+})
+
+
+
+export default App;
